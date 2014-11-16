efd770ae8a6c234cf48f8bfaa0639ebd767166e2
shsalt <shepards.alt@gmail.com>
2014-11-16 15:34:50 -0800

    take it easy

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
             $('.menu-link').bigSlide({
                 menuWidth: '8em'
             });
+            $('.slick-slides').slick({
+              dots: true,
+              infinite: true,
+              slidesToShow: 2,
+              })
+            
         });
