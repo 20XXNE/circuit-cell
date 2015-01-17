6a9310336cd50041d3e2fcd51d70f8d0929b9593
shsalt <shepards.alt@gmail.com>
2015-01-17 13:21:26 -0800

    why

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
-            $('.menu-link').bigSlide({
-                menuWidth: '8em'
-            });
-            $('.slick-slides').slick({
-              dots: true,
-              infinite: true,
-              slidesToShow: 2,
-              })
-            
-        });
+    $('.menu-link').bigSlide({
+            menuWidth: '8em'
+    });
+});
