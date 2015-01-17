211fe3e1614e5c55626ccaac87b88ffd75089dbb
shsalt <shepards.alt@gmail.com>
2015-01-17 14:35:22 -0800

    capstone death when

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
-            $('.menu-link').bigSlide({
-                menuWidth: '8em'
-            });
-            $('.slick-slides').slick({
-              dots: true,
-              infinite: true,
-              slidesToShow: 2,
-              })
-            
-        });
+    $('.menu-link').bigSlide({
+            menuWidth: '8em'
+    });
+    var s = slidr.create('slidr-img', {
+  breadcrumbs: true,
+  overflow: true
+}).add('h', ['one', 'two', 'three', 'four', 'five', 'one'])
+  .start();
+    /*slidr.create('slidr-img', {
+        breadcrumbs: true,
+        controls: 'corner',
+        fade: false,
+        keyboard: true,
+        overflow: true,
+        touch: true,
+        transition: 'fade'
+    }).start();*/
+});
